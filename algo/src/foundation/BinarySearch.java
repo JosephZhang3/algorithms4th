1801240c1d12c018041fcb1fbd118e3a3f872537
张江浩 <1294700694@qq.com>
2018-10-30 15:00:42 +0800

    结构调整+完成二分查找 so easy

---
+++ algo/src/foundation/BinarySearch.java
@@ -0,0 +1,31 @@
+package foundation;
+
+/**
+ * 二分查找算法，运用前提：输入的数据数组必须是已经排好序的
+ */
+public class BinarySearch {
+
+    private static int rank(int key, int[] a) {
+        int lo = 0;
+        int hi = a.length - 1;
+
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (key < a[mid]) {
+                hi = mid - 1;
+            } else if (key > a[mid]) {
+                lo = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+//        int[] whiteList =
+
+        int[] a = {8, 7, 4, 9, 11};
+        System.out.println(rank(9, a));
+    }
+}
