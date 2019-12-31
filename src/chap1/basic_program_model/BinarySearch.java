c2c6705e3e0bad1a8541e94cbf9a3fa48f346a24
张江浩 <1294700694@qq.com>
2020-01-01 01:00:03 +0800

    二分查找

--- src/chap1/basic_program_model/BinarySearch.java
+++ src/chap1/basic_program_model/BinarySearch.java
@@ -1,7 +1,7 @@
-package foundation;
+package chap1.basic_program_model;
 
 /**
- * 二分查找算法，使用的前提：输入的数据数组必须是 “已经排好序的”
+ * 二分查找算法，使用的前提：the data array gave must be 'ordered'
  */
 public class BinarySearch {
 
@@ -28,19 +28,22 @@ public class BinarySearch {
     //用递归方式实现
     private static int rankRecursive(int key, int[] a, int lo, int hi) {
 
-        int mid = lo + (hi - lo) / 2;//中点
-        System.out.println("首元素索引" + lo + "尾元素索引" + hi + "中点元素索引" + mid);
+        int mid = lo + (hi - lo) / 2;//中点索引，始终会是一个正整数或者零！
+        System.out.println("首元素索引" + lo + "，尾元素索引" + hi + "，中点元素索引" + mid);
 
-        if (lo <= hi) {
+        if (lo <= hi) {//新搜索起点或终点都是正常（在界内）
             if (key < a[mid]) {
+                //因为要查找的key元素比mid索引及之后位置的元素都要小，所以新的搜索终点变成mid - 1
                 hi = mid - 1;
             } else if (key > a[mid]) {
+                //因为要查找的key元素比mid索引及之前位置的元素都要大，所以新的搜索起点变成mid + 1
                 lo = mid + 1;
             } else {
+                //要查找的key元素和mid索引一样大，搜索命中返回
                 return mid;
             }
             return rankRecursive(key, a, lo, hi);
-        } else {
+        } else {/* 理解： lo > hi 即选取的新搜索起点或终点已经越界， */
             return -1;
         }
     }
@@ -51,8 +54,8 @@ public class BinarySearch {
      * @param args args
      */
     public static void main(String[] args) {
-        int[] a = {4, 7, 8, 9, 11};
+        int[] a = {4, 7, 8, 9, 11, 23};
 //        System.out.println(rank(8, a));
-        System.out.println(rankRecursive(9, a, 0, a.length - 1));
+        System.out.println("查找匹配元素索引" + rankRecursive(9, a, 0, a.length - 1));
     }
 }
---
+++ src/chap1/basic_program_model/StandardInputTest.java
@@ -0,0 +1,20 @@
+package chap1.basic_program_model;
+
+import util.StdIn;
+
+/**
+ * 计算标准输入给出的数的平均数
+ * Ctrl +D 结束输入
+ */
+public class StandardInputTest {
+
+    public static void main(String[] args) {
+        double sum = 0;
+        int cnt = 0;
+        while (!StdIn.isEmpty()) {
+            sum += StdIn.readDouble();
+            cnt++;
+        }
+        System.out.printf("Average is %.2f", sum / cnt);
+    }
+}
