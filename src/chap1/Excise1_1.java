7a43b6c1df493c744a07d8570fc1a5945111247e
张江浩 <1294700694@qq.com>
2020-01-06 01:30:19 +0800

    done 1.1.18

--- src/chap1/Excise1_1.java
+++ src/chap1/Excise1_1.java
@@ -246,6 +246,52 @@ public class Excise1_1 {
         return r;
     }
 
+    private static void exercise_1_1_16() {
+        String r = exR1(6);
+        System.out.println(r);
+    }
+
+    private static String exR1(int n) {
+        if (n <= 0) {
+            return "";
+        }
+
+        return exR1(n - 3) + n + exR1(n - 2) + n;
+    }
+
+
+    private static void exercise_1_1_18() {
+//        System.out.println(mystery(2, 25));
+//        System.out.println(mystery(3, 31));
+//        System.out.println(mystery(8, 80));
+
+        System.out.println(mysteryV2(2, 25));
+        System.out.println(mysteryV2(3, 11));
+        System.out.println(mysteryV2(8, 80));
+    }
+
+    private static int mystery(int a, int b) {
+        if (b == 0) {
+            return 0;
+        }
+        if (b % 2 == 0) {
+            return mystery(a + a, b / 2);
+        }
+
+        return mystery(a + a, b / 2) + a;
+    }
+
+    private static int mysteryV2(int a, int b) {
+        if (b == 0) {
+            return 1;
+        }
+        if (b % 2 == 0) {
+            return mysteryV2(a * a, b / 2);
+        }
+
+        return mysteryV2(a * a, b / 2) * a;
+    }
+
     public static void main(String[] args) {
 
         /*
@@ -272,8 +318,13 @@ public class Excise1_1 {
         exercise_1_1_14();
 
         exercise_1_1_15();
+
+        exercise_1_1_16();
+
+        exercise_1_1_18();
         */
 
 
     }
+
 }
