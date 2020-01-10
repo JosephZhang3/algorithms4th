6e477d217c250ba8fe47d152e14c52beb9856342
张江浩 <1294700694@qq.com>
2020-01-10 22:49:44 +0800

    part commit

--- src/chap1/basic_program_model/BinarySearch.java
+++ src/chap1/basic_program_model/BinarySearch.java
@@ -35,7 +35,6 @@ public class BinarySearch {
     private static int rankRecursive(int key, int[] a, int lo, int hi) {
 
         int mid = lo + (hi - lo) / 2;//中点索引，始终会是一个正整数或者零！
-        System.out.println("ran time once");
         System.out.println("首元素索引" + lo + "，尾元素索引" + hi + "，中点元素索引" + mid);
 
         if (lo <= hi) {//新搜索起点或终点都是正常（在界内）
@@ -61,7 +60,7 @@ public class BinarySearch {
      * @param args args
      */
     public static void main(String[] args) {
-        int[] a = {4, 7, 8, 9, 11, 23};
+//        int[] a = {4, 7, 8, 9, 11, 23};
 //        System.out.println(rank(8, a));
         //System.out.println("查找匹配元素索引" + rankRecursive(9, a, 0, a.length - 1));
 
@@ -69,6 +68,32 @@ public class BinarySearch {
         String param = args[1];
 
         Arrays.sort(whiteList);
+
+
+        //练习1.1.28 删除白名单中所有重复元素
+        int[] occurredArray = new int[whiteList.length];
+        int nowIndex = 0;
+        for (int i : whiteList) {
+            boolean occurredFlag = false;
+            for (int anOccurredArray : occurredArray) {
+                if (i == anOccurredArray) {
+                    occurredFlag = true;
+                }
+            }
+            if (!occurredFlag) {
+                occurredArray[nowIndex] = i;
+                ++nowIndex;
+            }
+        }
+        whiteList = occurredArray;
+        System.out.print("去重后白名单元素为\t");
+        for (int i : whiteList) {
+            System.out.print(i + "\t");
+        }
+        System.out.println();
+        //练习1.1.28 ... 代码结束
+
+
         while (!StdIn.isEmpty()) {
             int key = StdIn.readInt();
             int resultIndex = rankRecursive(key, whiteList, 0, whiteList.length);
